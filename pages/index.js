ca2c5f7b86cd3642f8eac9524507ce745f997eaa
Cheremukhin Stanislav <www.stasonline.ru@mail.ru>
2021-05-05 22:09:16 +0300

    Add antd clean proj

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import {DatePicker} from 'antd'
+import 'antd/dist/antd.css'
+import React from "react"
+import {ConfigProvider} from 'antd'
+import ruRu from 'antd/lib/locale/ru_RU'
+
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>TADOIT App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to TEMP
-        </h1>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  )
+    return (
+        <ConfigProvider locale={ruRu}>
+            <DatePicker/>
+        </ConfigProvider>
+    )
 }
