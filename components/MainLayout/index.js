6f2b93b4eb5685802553d3cccf6946080a7ed17a
Cheremukhin Stanislav <www.stasonline.ru@mail.ru>
2021-05-06 15:02:40 +0300

    Add basic meta to layout

--- components/MainLayout/index.js
+++ components/MainLayout/index.js
@@ -3,12 +3,23 @@ import React from "react"
 import {Layout} from 'antd'
 import styles from '/styles/header.module.scss'
 import Header from '/components/Header'
+import Head from "next/head"
 
 const {Content} = Layout
 
-export default function MainLayout({children}) {
+export default function MainLayout({children, title = 'Title'}) {
     return (
         <Layout>
+            <Head>
+                <title>{title}</title>
+
+                <meta charSet="utf-8"/>
+                <meta name='viewport' content='width=device-width,initial-scale=1'/>
+                <meta content='true' name='HandheldFriendly'/>
+                <meta content='width' name='MobileOptimized'/>
+                <meta content='yes' name='apple-mobile-web-app-capable'/>
+
+            </Head>
             <Header/>
             <Content className={styles.main}>
                 {children}
