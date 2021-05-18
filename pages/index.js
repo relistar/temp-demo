ba127388b491da98bf583d315a78ab061cd1d980
Stanislav Cheremukhin <www.stasonline.ru@mail.ru>
2021-05-19 02:32:59 +0300

    Add markup for most part

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,36 @@ import MainLayout from "../components/MainLayout"
 import Title from "antd/lib/typography/Title"
 import {Button, Checkbox, InputNumber} from 'antd'
 import Scrollbar from "react-scrollbars-custom"
-import { DeleteOutlined } from '@ant-design/icons'
+import { DeleteOutlined, FileTextOutlined } from '@ant-design/icons'
 
 export default function Home() {
+    const specLines = [
+        {
+            name: "Щит модуля",
+            qty: 1
+        },
+        {
+            name: "Щит модуля",
+            qty: 1
+        },
+        {
+            name: "Реле напряжения",
+            qty: 1
+        },
+        {
+            name: "Щит модуля",
+            qty: 1
+        },
+        {
+            name: "УЗО",
+            qty: 1
+        },
+        {
+            name: "Автомат",
+            qty: 1
+        }
+    ]
+
     return (
         <MainLayout>
             <section className="card">
@@ -26,7 +53,8 @@ export default function Home() {
                         <div className="empty empty--fourth"/>
                     </div>
                     <div className="card-main">
-                        <div className="card__image">
+                        <div className="card-image">
+                            <img src="/img/card-image.png" alt=""/>
                         </div>
                         <div className="card-info">
                             <div className="card-info__top">
@@ -52,7 +80,7 @@ export default function Home() {
                                         <span className="card-answer__value">Нет</span>
                                     </div>
                                 </div>
-                                <div className="card__subtitle">Параметры подобраны исходя из ответов в разделе
+                                <div className="card-subtitle">Параметры подобраны исходя из ответов в разделе
                                     “Конструктор”
                                 </div>
 
@@ -61,7 +89,7 @@ export default function Home() {
                                 <div className="card-info-quest">
                                     <div className="card-info-quest__head">
                                         Желаете ли Вы приобрести
-                                        дополнительные опции?
+                                        дополнительные опции? <br/>
                                         Это ни к чему не обязывает, просто
                                         мы рассчитаем с дополнительными
                                         функциями
@@ -75,51 +103,183 @@ export default function Home() {
                                 <div className="card-info-details">
                                     <div className="card-info-details__head">Спецификация электрощита</div>
                                     <div className="card-info-details__lines">
-                                        <Scrollbar style={{ width: 250, height: 250 }}>
-                                            <div className="card-info-details-line">
-                                                <span className="card-info-details-line__name">1. Щит модуля</span>
-                                                <InputNumber min={0} defaultValue={1} />
-                                                <Button icon={<DeleteOutlined/>}/>
-                                            </div>
-                                            <div className="card-info-details-line">
-                                                <span className="card-info-details-line__name">1. Щит модуля</span>
-                                                <InputNumber min={0} defaultValue={1} />
-                                                <Button icon={<DeleteOutlined/>}/>
-                                            </div>
-                                            <div className="card-info-details-line">
-                                                <span className="card-info-details-line__name">1. Щит модуля</span>
-                                                <InputNumber min={0} defaultValue={1} />
-                                                <Button icon={<DeleteOutlined />}></Button>
-                                            </div>
-                                            <div className="card-info-details-line">
-                                                <span className="card-info-details-line__name">1. Щит модуля</span>
-                                                <InputNumber min={0} defaultValue={1} />
-                                                <Button icon={<DeleteOutlined />}></Button>
-                                            </div>
-                                            <div className="card-info-details-line">
-                                                <span className="card-info-details-line__name">1. Щит модуля</span>
-                                                <InputNumber min={0} defaultValue={1} />
-                                                <Button icon={<DeleteOutlined />}></Button>
-                                            </div>
-                                            <div className="card-info-details-line">
-                                                <span className="card-info-details-line__name">1. Щит модуля</span>
-                                                <InputNumber min={0} defaultValue={1} />
-                                                <Button icon={<DeleteOutlined />}></Button>
-                                            </div>
-                                            <div className="card-info-details-line">
-                                                <span className="card-info-details-line__name">1. Щит модуля</span>
-                                                <InputNumber min={0} defaultValue={1} />
-                                                <Button icon={<DeleteOutlined />}></Button>
-                                            </div>
+                                        <Scrollbar style={{ width: '100%', height: 282 }}>
+
+                                            {specLines.map((line, index) => (
+                                                <div className="card-info-details-line">
+                                                    <span className="card-info-details-line__name">{index + 1}. {line.name}</span>
+                                                    <div className="card-info-details-line__controls">
+                                                        <div className="input-num">
+                                                            <span className="input-num-btn">-</span>
+                                                            <InputNumber min={0} defaultValue={2}  onChange={() => {}}/>
+                                                            <span className="input-num-btn">+</span>
+                                                        </div>
+                                                        <div className="remove-button">
+                                                            <Button icon={<DeleteOutlined />}/>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            ))}
                                         </Scrollbar>
                                     </div>
-                                    <div className="card-info-details__submitter">
-                                        <Button type="primary">
-                                            Сохранить
-                                        </Button>
-                                        <span className="submitter__label">
-                                            Нажимая кнопку, Вы сохраняете все изменения, произведенные в этом окне
+                                    <div className="card-info-details-submitter">
+                                        <div className="card-info-details-submitter__btn btn-wrap">
+                                            <Button type="primary">
+                                                Сохранить
+                                            </Button>
+                                        </div>
+                                        <span className="card-info-details-submitter__label">
+                                            Нажимая кнопку, Вы сохраняете все <br/> изменения, произведенные в этом окне
                                         </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="page-header page-header--second">
+                        <Title level={2}>
+                            Варианты электрощитов, подходящие под Ваши параметры
+                        </Title>
+                        <div className="empty empty--first"/>
+                        <div className="empty empty--second"/>
+                        <div className="empty empty--third empty--no-border"/>
+                        <div className="empty empty--fourth empty--no-border empty--relative">
+                            <svg className="ellipse--right-bottom" width="167" height="75" viewBox="0 0 169 76" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0.999998 76C0.999994 34.5787 38.3842 1.00001 84.5 1.00001C130.616 1 168 34.5786 168 76" stroke="#DCE8FF"/>
+                            </svg>
+                        </div>
+                    </div>
+                    <div className="spec-var-table">
+                        <div className="table-head">
+                            <div className="table-head__item table-head__item--first">Бренд/серия</div>
+                            <div className="table-head__item table-head__item--second">Стоимость</div>
+                            <div className="table-head__item table-head__item--third">Наличие</div>
+                            <div className="table-head__item table-head__item--fourth table-head__item--empty"/>
+                            <div className="table-head__item table-head__item--fifth table-head__item--empty"/>
+                        </div>
+                        <div className="table-lines">
+                            <div className="table-lines__item">
+                                <div className="table-lines__col table-lines__col--first">
+                                    <span className="table-lines__brand">ABB</span>
+                                    &nbsp;
+                                    <span className="table-lines__serial">премиум</span>
+                                </div>
+                                <div className="table-lines__col table-lines__col--second">
+                                    <span className="table-lines__price">29'920 &#8381;</span>
+                                </div>
+                                <div className="table-lines__col table-lines__col--third">
+                                    <div className="table-lines-availability table-lines-availability--true">
+                                        В наличии
+                                    </div>
+                                </div>
+                                <div className="table-lines__col table-lines__col--fourth">
+                                    <div className="btn-wrap">
+                                        <Button>Скачать</Button>
+                                    </div>
+                                </div>
+                                <div className="table-lines__col table-lines__col--fifth">
+                                    <div className="btn-wrap btn-wrap--icon">
+                                        <Button icon={<FileTextOutlined />}>Заказать</Button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="table-lines__item">
+                                <div className="table-lines__col table-lines__col--first">
+                                    <span className="table-lines__brand">ABB</span>
+                                    &nbsp;
+                                    <span className="table-lines__serial">премиум</span>
+                                </div>
+                                <div className="table-lines__col table-lines__col--second">
+                                    <span className="table-lines__price">29'920 &#8381;</span>
+                                </div>
+                                <div className="table-lines__col table-lines__col--third">
+                                    <div className="table-lines-availability table-lines-availability--true">
+                                        В наличии
+                                    </div>
+                                </div>
+                                <div className="table-lines__col table-lines__col--fourth">
+                                    <div className="btn-wrap">
+                                        <Button>Скачать</Button>
+                                    </div>
+                                </div>
+                                <div className="table-lines__col table-lines__col--fifth">
+                                    <div className="btn-wrap">
+                                        <Button icon={<FileTextOutlined />}>Заказать</Button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="table-lines__item">
+                                <div className="table-lines__col table-lines__col--first">
+                                    <span className="table-lines__brand">ABB</span>
+                                    &nbsp;
+                                    <span className="table-lines__serial">премиум</span>
+                                </div>
+                                <div className="table-lines__col table-lines__col--second">
+                                    <span className="table-lines__price">29'920 &#8381;</span>
+                                </div>
+                                <div className="table-lines__col table-lines__col--third">
+                                    <div className="table-lines-availability table-lines-availability--true">
+                                        В наличии
+                                    </div>
+                                </div>
+                                <div className="table-lines__col table-lines__col--fourth">
+                                    <div className="btn-wrap">
+                                        <Button>Скачать</Button>
+                                    </div>
+                                </div>
+                                <div className="table-lines__col table-lines__col--fifth">
+                                    <div className="btn-wrap">
+                                        <Button icon={<FileTextOutlined />}>Заказать</Button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="table-lines__item">
+                                <div className="table-lines__col table-lines__col--first">
+                                    <span className="table-lines__brand">ABB</span>
+                                    &nbsp;
+                                    <span className="table-lines__serial">премиум</span>
+                                </div>
+                                <div className="table-lines__col table-lines__col--second">
+                                    <span className="table-lines__price">29'920 &#8381;</span>
+                                </div>
+                                <div className="table-lines__col table-lines__col--third">
+                                    <div className="table-lines-availability table-lines-availability--true">
+                                        В наличии
+                                    </div>
+                                </div>
+                                <div className="table-lines__col table-lines__col--fourth">
+                                    <div className="btn-wrap">
+                                        <Button>Скачать</Button>
+                                    </div>
+                                </div>
+                                <div className="table-lines__col table-lines__col--fifth">
+                                    <div className="btn-wrap">
+                                        <Button icon={<FileTextOutlined />}>Заказать</Button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="table-lines__item">
+                                <div className="table-lines__col table-lines__col--first">
+                                    <span className="table-lines__brand">ABB</span>
+                                    &nbsp;
+                                    <span className="table-lines__serial">премиум</span>
+                                </div>
+                                <div className="table-lines__col table-lines__col--second">
+                                    <span className="table-lines__price">29'920 &#8381;</span>
+                                </div>
+                                <div className="table-lines__col table-lines__col--third">
+                                    <div className="table-lines-availability table-lines-availability--true">
+                                        В наличии
+                                    </div>
+                                </div>
+                                <div className="table-lines__col table-lines__col--fourth">
+                                    <div className="btn-wrap">
+                                        <Button>Скачать</Button>
+                                    </div>
+                                </div>
+                                <div className="table-lines__col table-lines__col--fifth">
+                                    <div className="btn-wrap">
+                                        <Button icon={<FileTextOutlined />}>Заказать</Button>
                                     </div>
                                 </div>
                             </div>
