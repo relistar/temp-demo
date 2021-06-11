f43dc27beb34083b2e0e9705ef35dd7d6524f517
Cheremukhin Stanislav <www.stasonline.ru@mail.ru>
2021-06-12 00:25:30 +0300

    experiment

--- bapi/manual.js
+++ bapi/manual.js
@@ -88,31 +88,31 @@ export const BASE_API = {
 
 export const API = {
     getCharacteristicsByCategoryId: (categoryId) => {
-        return rootApi.get(`/characteristics/${categoryId}/`)
+        return rootApi.get(`/characteristics/${categoryId}`)
     },
     postSpec: (spec) => {
-        return jsonRootApi.post('/detail/manual_spec_create/', spec)
+        return jsonRootApi.post('/detail/manual_spec_create', spec)
     },
     updateSpecLine: (payload) => {
-        return jsonRootApi.put('/spec_lines/update_spec_line/', payload)
+        return jsonRootApi.put('/spec_lines/update_spec_line', payload)
     },
     getBuildDetailsById: (specId) => {
         return rootApi.get(`/details_agg/${specId}`)
     },
     postQuiz(payload) {
-        return jsonRootApi.post('/detail/quiz_result/', payload)
+        return jsonRootApi.post('/detail/quiz_result', payload)
     },
     postSpecForm(spec) {
-        return jsonRootApi.put('/detail/quiz_result_upd/', spec)
+        return jsonRootApi.put('/detail/quiz_result_upd', spec)
     },
     createOrderByDetail(detail) {
-        return jsonRootApi.post('/order_headers/selected_detail/', detail)
+        return jsonRootApi.post('/order_headers/selected_detail', detail)
     },
     downloadSpecFileByDetail(payload) {
-        return jsonRootApi.post('/detail/specification/pdf/', payload, {responseType: 'blob'})
+        return jsonRootApi.post('/detail/specification/pdf', payload, {responseType: 'blob'})
     },
     loginByCredentials(payload) {
-        return jsonRootApi.post('/login/access-token/', qs.stringify(payload), {
+        return jsonRootApi.post('/login/access-token', qs.stringify(payload), {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
